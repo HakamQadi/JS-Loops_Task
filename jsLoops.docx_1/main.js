fa3cbac60016fb7e762b66f7bf571b1679eff649
HakamQadi <hakam.h.qadi@gmail.com>
2023-04-29 23:39:11 +0300

    done

--- jsLoops.docx_1/main.js
+++ jsLoops.docx_1/main.js
@@ -1,232 +1,230 @@
-// // Ex.1
+// Ex.1
 
-// for (let i = 1; i <= 10; i++) {
-//     console.log(i)
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
-// }
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+// Ex.2
+var counter = 1;
+while (counter <= 10) {
+  console.log(counter);
+  counter++;
+}
 
-// // Ex.2
-// var counter = 1;
-// while (counter <= 10) {
-//   console.log(counter);
-//   counter++;
-// }
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+// Ex.3
+var array = [1, 2, 3, 4, 5];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
 
-// // Ex.3
-// var array = [1, 2, 3, 4, 5];
-// for (let i = 0; i < array.length; i++) {
-//   console.log(array[i]);
-// }
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+// Ex.4
 
-// // Ex.4
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
 
-// for (let i = 0; i <= 10; i++) {
-//   if (i % 2 == 0) {
-//     console.log(i);
-//   }
-// }
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+// Ex.5
 
-// // Ex.5
+var sum = 0;
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+console.log(sum);
 
-// var sum = 0;
-// for (let i = 1; i <= 10; i++) {
-//   sum += i;
-// }
-// console.log(sum);
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+// Ex.6
+var array = [1, 2, 3, 4, 5];
+var max = array[0];
 
-// // Ex.6
-// var array= [1, 2, 3, 4, 5];
-// var max=array[0];
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > max) {
+    max = array[i];
+  }
+}
+console.log(max);
 
-// for (let i = 0; i < array.length; i++) {
-//     if (array[i]>max) {
-//         max=array[i]
-//     }
-// }
-// console.log(max)
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+// Ex.7
+var array = [5, 4, 3, 2, 1];
+var min = array[0];
 
-// // Ex.7
-// var array= [5, 4, 3, 2, 1];
-// var min=array[0];
+for (let i = 0; i < array.length; i++) {
+  if (array[i] < min) {
+    min = array[i];
+  }
+}
+console.log(min);
 
-// for (let i = 0; i < array.length; i++) {
-//     if (array[i]<min) {
-//         min=array[i]
-//     }
-// }
-// console.log(min)
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+// Ex.8
 
-// // Ex.8
+var array = [1, 2, 3, 4, 5];
+var sum = 0;
+for (let i = 0; i < array.length; i++) {
+  sum += array[i];
+}
+console.log(sum / array.length);
 
-// var array = [1, 2, 3, 4, 5];
-// var sum = 0;
-// for (let i = 0; i < array.length; i++) {
-//   sum += array[i];
-// }
-// console.log(sum / array.length);
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+// Ex.9
 
-// // Ex.9
+var fac = 1;
+var num = 5;
+for (let i = 1; i <= num; i++) {
+  fac *= i;
+}
+console.log(fac);
 
-// var fac=1;
-// var num=5;
-// for (let i = 1; i <= num; i++) {
-//    fac*=i;
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// }
-// console.log(fac)
+// Ex.10
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+const num = 10;
+let a = 0,
+  b = 1,
+  c;
 
-// // Ex.10
+for (let i = 0; i < num; i++) {
+  if (i <= 1) {
+    c = i;
+  } else {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  console.log(c);
+}
 
-// const num = 10;
-// let a = 0, b = 1, c;
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// for (let i = 0; i < num; i++) {
-//   if (i <= 1) {
-//     c = i;
-//   } else {
-//     c = a + b;
-//     a = b;
-//     b = c;
-//   }
-//   console.log(c);
-// }
+// Ex.11
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+const num = 20;
 
-// // Ex.11
+for (let i = 2; i <= num; i++) {
+  let isPrime = true;
 
-// const num = 20;
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      isPrime = false;
+      break;
+    }
+  }
 
-// for (let i = 2; i <= num; i++) {
-//   let isPrime = true;
+  if (isPrime) {
+    console.log(i);
+  }
+}
 
-//   for (let j = 2; j < i; j++) {
-//     if (i % j === 0) {
-//       isPrime = false;
-//       break;
-//     }
-//   }
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-//   if (isPrime) {
-//     console.log(i);
-//   }
-// }
+// Ex.12
+var num = 5;
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+for (let i = 1; i <= 10; i++) {
+  console.log(num + " X " + i + " = " + num * i);
+}
 
-// // Ex.12
-// var num = 5;
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// for (let i = 1; i <= 10; i++) {
-//   console.log(num + " X " + i + " = " + num * i);
-// }
+// Ex.13
+var array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    console.log(array[i][j]);
+  }
+}
 
-// // Ex.13
-// var array = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-// ];
+console.log(" ");
+console.log("*************");
+console.log(" ");
 
-// for (let i = 0; i < array.length; i++) {
-//     for (let j = 0; j < array.length; j++) {
-//         console.log(array[i][j])
+// Ex.14
+var array = [1, 2, 3, 4, 5];
+for (let i = array.length; i > 0; i--) {
+  console.log(i);
+}
 
-//     }
+console.log(" ");
+console.log("*************");
+console.log(" ");
+// Ex.15
 
-// }
+var array = [1, 2, 3, 4, 5];
+var first = 2;
+var last = 4;
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
+for (let i = first; i <= last; i++) {
+  console.log(array[i]);
+}
 
-// // Ex.14
-// var array = [1, 2, 3, 4, 5];
-// for (let i = array.length; i > 0; i--) {
-//   console.log(i);
-// }
+console.log(" ");
+console.log("*************");
+console.log(" ");
+// Ex.16
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
-// // Ex.15
+var array = [1, 2, 3, 4, 5];
+var num = 2;
+for (let i = 0; i < array.length; i += num) {
+  console.log(array[i]);
+}
 
-// var array = [1, 2, 3, 4, 5];
-// var first = 2;
-// var last = 4;
+console.log(" ");
+console.log("*************");
+console.log(" ");
+// Ex.17
 
-// for (let i = first; i <= last; i++) {
-//   console.log(array[i]);
-// }
+var array = [1, 2, 3, 4, 5];
+var num = 4;
 
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
-// // Ex.16
-
-// var array=[1, 2, 3, 4, 5];
-// var num=2;
-// for (let i = 0; i < array.length; i+=num) {
-//     console.log(array[i])
-// }
-
-// console.log(" ");
-// console.log("*************");
-// console.log(" ");
-// // Ex.17
-
-// var array = [1, 2, 3, 4, 5];
-// var num = 4;
-
-// for (let i = 0; i < array.length; i++) {
-//   if (array[i] == num) {
-//     console.log("true");
-//   }
-// }
+for (let i = 0; i < array.length; i++) {
+  if (array[i] == num) {
+    console.log("true");
+  }
+}
 
 console.log(" ");
 console.log("*************");
